071e6091fccb2bbb006e6799bae94167f7d01f83
sb <sb@paris>
2020-10-30 14:39:31 +0100

    minor changes

--- start_ck_rc3.m
+++ start_ck_rc3.m
@@ -1,7 +1,7 @@
 % Shortcut summary goes here
 SPM_DIR="/opt/spm";
-SB_TOOLS="/opt/sb_tools"
-CK_DIR="/opt/ck_rc3"
+SB_TOOLS="/opt/sb_tools";
+CK_DIR="/opt/ck_rc3";
 
 addpath(fullfile(SB_TOOLS, 'sb_preprocesser'));
 addpath(fullfile(SB_TOOLS, 'sb_roi_cluster_manager'));
